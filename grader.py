1e5139326cf2a3e10d2c1d5d352cdb208f1edb74
Daniel Loera <danny.reatret@gmail.com>
2018-02-21 19:47:49 -0600

    Added Rubric class. Refactored pac.py.

--- grader.py
+++ grader.py
@@ -2,6 +2,31 @@ import os
 import subprocess
 
 
+class PythonRubric:
+
+    def __init__(self, outputs, schemes, max_score):
+        # list of list of expected program lines.
+        self.outputs = outputs
+        # list of list of grade values of each line
+        # corresponding to the output.
+        self.schemes = schemes
+        self.max_score = max_score
+
+    def grade(self, user_outputs):
+        grade = self.max_score
+        for i in range(self.outputs):
+            user_output = user_output[i]
+            expected_output = self.outputs[i]
+            scheme = self.schemes[i]
+            for j in range(expected_output):
+                user_line = user_output[j]
+                expected_line = expected_output[j]
+                if user_line != expected_line:
+                    grade -= scheme[j]
+        return grade
+
+
+
 class PythonGrader:
 
     def __init__(self, submissions_dir, users, assignment):
@@ -48,7 +73,7 @@ class PythonGrader:
             user_str = user.name + "(" + str(user.id) + ")"
             if not os.path.isfile(submission_filename):
                 grades[user] = self.default_grade
-                print(user_str, "has no submission. Default grade.")
+                print(user_str, "has no submission.")
                 continue
             proc = self.__createProc__('python3', submission_filename)
             output, err = proc.communicate(input=self.args)
@@ -59,7 +84,7 @@ class PythonGrader:
                 if err != "":
                     grades[user] = self.default_grade
                     print(user_str,
-                        "program fails in python2 and python3. Default grade.\n")
+                        "program fails in python3 and python2.\n")
                     continue
             grades[user] = self.__evaluateGrade__(output.decode("utf-8"))
         return grades
