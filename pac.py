08c1d8def8f1438dac65bb46b1a64e8f762a1fc2
Daniel Matthew Loera <danielloera@utexas.edu>
2018-02-21 20:49:43 -0600

    Rubric creation and insertion into PythonGrader. Refactoring for grader incomming.

--- pac.py
+++ pac.py
@@ -1,14 +1,32 @@
 from canvashelper import CanvasHelper
 from grader import PythonGrader
+from grader import PythonRubric
+import os
 from termcolor import colored
 
-grading_term = "Spring 2018"
+GRADING_TERM = "Spring 2018"
+
+EXPECTED_OUTPUT_FILE = "expected.txt"
+SCHEME_FILE = "scheme.txt"
+
+def file_to_collections(file):
+    collections = []
+    collection = []
+    lines = file.readlines()
+    for line in lines:
+        if line == "\n":
+            collections.append(list(collection))
+            collection = []
+            continue
+        collection.append(line.strip())
+    collections.append(collection)
+    return collections
 
 def main():
-    # Initial information gathering
+    # Initial information collection
     ch = CanvasHelper()
     print("\nPython Autograder for Canvas")
-    print("Grading for {}".format(grading_term))
+    print("Grading for {}".format(GRADING_TERM))
 
     ch.showCourseSelection()
     course_selection = int(input("\nWhich course would you like to select? "))
@@ -25,8 +43,39 @@ def main():
     users = ch.getUsers()
     assignment = ch.getAssignment()
 
+    # Rubric collection
+    outputs = None
+    schemes = None
+    print("Gathering expected outputs...")
+    if os.path.isfile(EXPECTED_OUTPUT_FILE):
+        outputs = file_to_collections(open(EXPECTED_OUTPUT_FILE))
+    else:
+        print(("Please create an output file\n"
+               "containing the following format as an example:\n"
+               "Hello This is my output.\n"
+               "This is only the first output.\n"
+               "\n"
+               "Now begins the second output.\n"
+               "Goodbye."))
+        raise Exception(
+                "Expected Output file {} does not exist.".format(
+                        EXPECTED_OUTPUT_FILE))
+
+    print("Gathering schemes...")
+    if os.path.isfile(SCHEME_FILE):
+        schemes = file_to_collections(open(SCHEME_FILE))
+        raise Exception("ss")
+    else:
+        print(("Please create a scheme file\n"
+               "containing the following format as an example:\n"
+               "1\n20\n\n15\n44"))
+        raise Exception(
+                "Expected scheme file {} does not exist.".format(
+                        SCHEME_FILE))
+    pr = PythonRubric(outputs, schemes, assignment.points_possible)
+
     # Grading
-    pg = PythonGrader(submission_directory, users, assignment)
+    pg = PythonGrader(submission_directory, users, assignment, pr)
     line_count_grading = int(input("\n0: Output Grading\n1: Line Count Grading\n"))
     if line_count_grading:
         count = int(input("Max amount of lines: "))
